e643e1aa0c45e1d6977fe6cf4748eae3477c5ce0
burak-biyikli <burak.biyikli@yahoo.com>
2024-02-25 15:38:07 -0600

    Minor formating changes

--- SRC/Scripts/AnimatedCanvas1.js
+++ SRC/Scripts/AnimatedCanvas1.js
@@ -4,38 +4,48 @@ class Point {
 		this.ypos = ypos;
 		this.xvel = xvel;
 		this.yvel = yvel;
-		this.size = size;
+		this.MaxSize = size;
 		this.ottl = ttl ;
 		this.cttl = ttl ;
-		this.Neighbors = [];
+		if (this.xvel==0 && this.yvel==0){
+			this.xvel=gR(-1,1)*AnimatedCanvas1.TimeScale;this.yvel=gR(-1,1)*AnimatedCanvas1.TimeScale;
+		}
+
 	}
-	update( othersPoints ) { 
+	get size(){
+		return this.intensity()*this.MaxSize;
+	}
+	update() { 
 		this.xpos+=this.xvel;
-		this.ypos+=this.ypos;
+		this.ypos+=this.yvel;
 		this.cttl--;
-	
-		// this.Neighbors = [];
-		// var dist = 0;
-		// for(i = 0; i<othersPoints.length; i++){
-		// 	dist = (this.xpos - othersPoints[i].xpos())^2 + (this.ypos - othersPoints[i].ypos())^2;
-		// 	if( dist < size*100) {
-		// 		this.Neighbors.push(othersPoints[i]);
-		// 	}
-		// }
 	}
 	intensity(){
-		return sqrt(-(this.cttl)(this.cttl - this.ottl)/ Math.pow(this.ottl/2, 2));
+		return Math.sqrt(-(this.cttl)*(this.cttl - this.ottl)/ Math.pow(this.ottl/2, 2));
 	}
 }
 
 var	AnimatedCanvas1 = {
 	AllPoints : [],
-	density: .001,
+	minSize: 3,
+	maxSize: 8,
+	density: .0001,
 	entityCap: 0,
-	TicksPerSecond: 1,
+	TicksPerSecond: 30,
+	TimeScale: .3,
 	MSPerTick: 0,//1000/AnimatedCanvas1.TicksPerSecond,
-	AvgTTl: 10,
+	AvgTTl: 16,
+	minRGB: 0x0F8A88,
+	maxRGB: 0xc0ffee,
+	linkLength: 96,
 	ATL: 0 //AnimatedCanvas1.AvgTTl*AnimatedCanvas1.TicksPerSecond
+}
+	
+function sRGB( min, max, ratio ){
+	delta = max-min;
+	tempString = (min+(((delta&0xff0000)*ratio)&0xff0000)+(((delta&0x00ff00)*ratio)&0x00ff00)+(((delta&0x0000ff)*ratio)&0x0000ff)).toString(16);
+	while (tempString.length < 6) { tempString = "0"+ tempString;}
+	return "#"+ tempString;
 }
 
 function gR(low, high){
@@ -56,8 +66,13 @@ function EnableAnimatedCanvas1( CanvasElementID ){
 
 function AnimateCanvas1( canvas, ctx ){
 
-	canvas.width = canvas.scrollWidth;
-	canvas.height = canvas.scrollHeight;
+	//Clear canvas and update internal size as needed
+	if( canvas.width != canvas.scrollWidth || canvas.height != canvas.scrollHeight){
+		canvas.width = canvas.scrollWidth;
+		canvas.height = canvas.scrollHeight;
+	} else {
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+	}
 	AnimatedCanvas1.entityCap = canvas.height*canvas.width*AnimatedCanvas1.density;
 	
 	//Cull 
@@ -72,21 +87,58 @@ function AnimateCanvas1( canvas, ctx ){
 
 	//Create
 	while( AnimatedCanvas1.AllPoints.length < AnimatedCanvas1.entityCap ) {
-		AnimatedCanvas1.AllPoints.push( new Point( gR( 10, canvas.width), gR(10, canvas.height), gR(-2,2), gR(-2,2), gR(2,5), gR(AnimatedCanvas1.ATL/2,AnimatedCanvas1.ATL*1.5)) );	
+		AnimatedCanvas1.AllPoints.push( new Point( gR( 10, canvas.width), gR(10, canvas.height), gR(-1,1)*AnimatedCanvas1.TimeScale, gR(-1,1)*AnimatedCanvas1.TimeScale, gR(AnimatedCanvas1.minSize,AnimatedCanvas1.maxSize), gR(AnimatedCanvas1.ATL/2,AnimatedCanvas1.ATL*1.5)) );	
 	}
 
-	for(i =0; i<AnimatedCanvas1.AllPoints.length; i++){
-		AnimatedCanvas1.AllPoints[i].update( [] );
+	//Update
+	var dict = {}; //Key Format (int) = x+y*canvas.width
+	var div = AnimatedCanvas1.linkLength;
+	var lim = div*div;
+	var ymult = Math.floor(canvas.width/div);
+
+	for(i=0; i<AnimatedCanvas1.AllPoints.length; i++){
+		AnimatedCanvas1.AllPoints[i].update();
+		var r = AnimatedCanvas1.AllPoints[i];
+		var index = Math.floor(r.xpos/div)+ymult*Math.floor(r.ypos/div);
+		dict[index] = dict[index] || [];
+		dict[index].push( [r.xpos, r.ypos, r.intensity()] )
+
+		var dx,dy;
+		for(dx=-1;dx<2;dx++){
+			for(dy=-1;dy<2;dy++){
+				if(dx==0&&dy==0) {continue;}
+				if( dict[index+dx+dy*ymult] ) {
+					dict[index+dx+dy*ymult].push( [r.xpos, r.ypos, r.intensity()] )
+				}
+			}
+		}
+	}
+
+	for (var key of Object.keys(dict)) {
+		var points = dict[key];
+		var p1,p2;
+		for(p1=0; p1<points.length;p1++){
+			for(p2=p1+1; p2<points.length;p2++){
+				var dist = Math.pow(points[p1][0]-points[p2][0],2) + Math.pow(points[p1][1]-points[p2][1],2); 
+				if( dist <= lim){
+					ctx.beginPath();
+					ctx.strokeStyle = sRGB(AnimatedCanvas1.minRGB, AnimatedCanvas1.maxRGB, Math.min(Math.pow(1-dist/lim, 3), points[p1][2],points[p2][2]) );
+					ctx.moveTo(points[p1][0], points[p1][1]);
+					ctx.lineTo(points[p2][0], points[p2][1]);
+					ctx.stroke();
+				}
+			}
+		}
 	}
 
 	//Show the state
-	for(i =0; i<AnimatedCanvas1.entityCap; i++){
+	for(i=0; i<AnimatedCanvas1.entityCap; i++){
 		ctx.beginPath();
 		ctx.arc( AnimatedCanvas1.AllPoints[i].xpos, AnimatedCanvas1.AllPoints[i].ypos, AnimatedCanvas1.AllPoints[i].size, 0, 2 * Math.PI, false);
-		ctx.fillStyle = 'blue';
+		ctx.fillStyle = sRGB(AnimatedCanvas1.minRGB, AnimatedCanvas1.maxRGB, AnimatedCanvas1.AllPoints[i].intensity() *.9 );
 		ctx.fill();
 		ctx.lineWidth = 2;
-		ctx.strokeStyle = 'black';
+		ctx.strokeStyle = sRGB(AnimatedCanvas1.minRGB, AnimatedCanvas1.maxRGB, AnimatedCanvas1.AllPoints[i].intensity() );
 		ctx.stroke();
 	}
 
